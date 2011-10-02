8f8a36f7acf406c063ccd0406e4f4c12b9fdbc74
Andreas Del Galdo <andreas.delgaldo@googlemail.com>
2011-10-02 20:27:36 +0200

    Creating a new structure for the repository  - src  - docs  - release

---
+++ src/admin.php
@@ -0,0 +1,60 @@
+<?php
+
+$cwd = getcwd();
+require_once ($cwd . "/conf/imageblog.conf.php");
+require_once ($cwd . "/tpl/".$imageblog['template'].".conf.php");
+
+require_once ($cwd . "/lib/imageblog-0.1.php");
+
+//get template settings for picture transformation
+$template = $GLOBALS[$imageblog['template']];
+
+/**** HEADER ****/ 
+print("<html><head><title>ImageBlog Admin Interface</title>\n");
+
+
+/**** CONTENT ****/ 
+
+if ($_FILES['entry_file']['error'] == 0) {
+    //create redirection
+    print("<meta http-equiv=\"refresh\" content=\"5;url=index.php\"></head><body>");
+
+    //file already uploaded --> create the blog entry
+    $entry_date  = date($imageblog['date_format']);
+    $entry_title = $_POST['entry_title'];
+    $entry_tags  = $_POST['entry_tags'];
+    $entry_file  = $imageblog['data_path']."/".$_FILES['entry_file']['name'];
+    
+    //copy the file to the data dircetory
+    move_uploaded_file($_FILES["entry_file"]["tmp_name"], $cwd."/".$entry_file);
+    
+    //create blog entry
+    $ImageBlogInstance = new ImageBlog();
+    $ImageBlogInstance->write_post($cwd, array('entry_date' => $entry_date, 'entry_title' => $entry_title, 'entry_tags' => $entry_tags, 'entry_file' => $entry_file), $template['image_size']);
+
+    //reset data
+    $entry_date  = "";
+    $entry_title = "";
+    $entry_tags  = "";
+    $entry_file  = "";
+
+    reset($_FILES['entry_file']);
+
+    print ("The entry ".$entry_title." successfully created.<br>You will be redirected in 5s...");
+} else {
+    print("</head><body><form  enctype=\"multipart/form-data\" action=\"admin.php\" method=\"post\" target=\"_self\">\n
+    <table>
+    <!-- hier folgen die Formularelemente -->
+    <tr><td>Title:</td><td><input type=\"text\" name=\"entry_title\" /></td></tr>
+    <tr><td>Tags:</td><td><input type=\"text\" name=\"entry_tags\" /></td></tr>
+    <trd><td>Image File:</td><td><input type=\"file\" name=\"entry_file\" /></td></tr>
+    <tr><td colspan=\"2\"><input type=\"submit\" value=\"create\"></td></tr>
+    </table>
+    
+    </form>");
+}
+
+/**** FOOTER ****/ 
+print("</body></html>\n");
+
+?>
---
+++ src/index.php
@@ -0,0 +1,83 @@
+<?php
+
+/****************************************************
+ * INFO: set global settings
+ ****************************************************/
+$cwd = getcwd();        //< get current working directory
+
+
+/****************************************************
+ * INFO: load main functionality and settings
+ *       The main library is loaded containing the
+ *       functionality to upload / share images
+ *
+ *       also settings and templates are loaded 
+ ****************************************************/
+if ((file_exists($cwd. "/lib/imageblog-0.1.php")) && (file_exists($cwd . "/conf/imageblog.conf.php"))) {
+    require_once ($cwd . "/lib/imageblog-0.1.php");
+    require_once ($cwd . "/conf/imageblog.conf.php");
+    
+    //read template
+    require_once($cwd . "/tpl/" . $imageblog['template'] . ".conf.php");
+    if (sizeof($GLOBALS[$imageblog['template']]) > 0) {
+
+        $template = $GLOBALS[$imageblog['template']];
+    }
+
+    //read command line parameter -> transfer to get (normally used with browser window)
+    if(isset($argv[1])) {
+        $_GET['page'] = $argv[1];
+    }
+    
+    //read the param
+    $page = $_GET['page'];
+    if (sizeof($page) == 0) $page = 0;
+
+
+    //make instance of application
+    $ImageBlogInstance = new ImageBlog();
+    
+    //get available posts
+    $entryList = $ImageBlogInstance->read_post($cwd); 
+    
+    /**** EXAMPLE HOW TO WRITE A POST ****/
+    //$ImageBlogInstance->write_post($cwd, array('entry_date' => "today", 'entry_title' => "write test", 'entry_tags' => "", 'entry_file' => "data/test.jpg"), 400);
+    
+} else {
+    print_r("Error running ImageBlog.\n");
+    exit();
+}
+
+
+/****************************************************
+ * INFO: build the website
+ *       the website frame and content is generated
+ *       by using ->create_html from the ImageBlog-
+ *       Library
+ ****************************************************/
+
+ 
+/**** HEADER ****/ 
+print("<html>\n");
+print("<head><title>".$imageblog['title']."</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"tpl/".$template['stylesheet']."\" /></head>\n\n");
+
+
+
+/**** CONTENT (SITE VISE) ****/
+//TODO: Extend template to deal with more entries per page
+if (sizeof($entryList) > 0) {
+    if ($page > sizeof($entryList) - 1) $page = 0;
+    if ($page == sizeof($entryList) - 1) $prev = -1; else $prev = $page + 1;
+    if ($page == 0) $next = -1; else $next = $page - 1;
+    
+    print $ImageBlogInstance->create_html($entryList[$page], $prev, $next);
+} else {
+    print("<br><br><br><div class=\"banner\">Open the Admin Interface to start a new ImageBlog</div>");
+}
+
+/**** FOOTER ****/
+print("\n<div class=\"footer\">Powered by ImageBlog</div>\n");
+print("</html>\n");
+
+
+?>
